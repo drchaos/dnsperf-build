030ac70037f631a5316a8ca22d54295cada27228
Yura Sheliah <yura.sheliah@secucloud.com>
2018-09-19 15:57:09 +0200

    fixed openssl build

--- dnsperf.nix
+++ dnsperf.nix
@@ -1,10 +1,11 @@
-{ fetchgitPrivate, stdenv , autoreconfHook, bind, openssl, libcap, libkrb5, libxml2, geoip }:
+{ fetchgit, stdenv , autoreconfHook, pkgconfig, bind, openssl, libcap, libkrb5, libxml2, geoip }:
 
 stdenv.mkDerivation {
   name = "dnsperf-0.0.1";
-  nativeBuildInputs = [ autoreconfHook ];
+  nativeBuildInputs = [ autoreconfHook pkgconfig ];
+  buildEnv = [openssl];
   buildInputs = [ openssl bind libcap libkrb5 libxml2 geoip ];
-  src = fetchgitPrivate {
+  src = fetchgit {
     url = https://github.com/drchaos/dnsperf-tcp;
     rev = "1ef861d71581c45de6262a71aedbf93f8a3adb92";
     sha256 = "1sb8nwn1h2vkyf319z89azgrahig324yvwj0whyxlg3gwinafppf";
--- release.nix
+++ release.nix
@@ -2,10 +2,10 @@ let pkgs    = import <nixpkgs> { inherit config; };
 
     config  = {
       allowUnfree = true;
-      packageOverrides = pkgs: rec {
+      #packageOverrides = pkgs: rec {
           #openssl = pkgs.openssl_1_1_0;
-          openssl = pkgs.openssl;
-      };
+          #openssl = pkgs.lib.meta.addMetaAttrs  { outputsToInstall = "dev"; } pkgs.openssl;
+      #};
     };
 
     callPackage = pkgs.lib.callPackageWith (pkgs // self );
